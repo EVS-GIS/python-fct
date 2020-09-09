578eac6285e6c1a9433e3acc61792dc4e43503d2
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2020-09-09 12:24:25 +0200

    Move swath code to separate module

--- fct/swath/__init__.py
+++ fct/swath/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 """
-Plotting & Data Vizualization
+Swath Profiles
 
 ***************************************************************************
 *                                                                         *
