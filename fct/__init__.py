d08d1214b6154f208fb14e1551cb6cdcd4a02422
Christophe ROUSSON <c.rousson.gpk@gmail.com>
2021-04-11 23:27:29 +0200

    Moving places

--- fct/__init__.py
+++ fct/__init__.py
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 """
 Fluvial Corridor Toolbox
 
--- fct/swath/__init__.py
+++ fct/swath/__init__.py
@@ -1,16 +1,5 @@
-# coding: utf-8
-
 """
 Swath Profiles
-
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 3 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
 """
 
 from .SwathMeasurement import nearest_value_and_distance
